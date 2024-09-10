2c4f6ec8f79d325256d8785374cc108adbef0e83
Martina Kay Basquez <martinabasquez@gmail.com>
2024-09-09 19:34:09 -0500

    to do: literally everything
    completed: displays list/decide on which radio button you want/styling needs work

--- AngularToDo/src/app/app.component.ts
+++ AngularToDo/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, numberAttribute } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToDo } from './models/to-do';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,13 +16,15 @@ export class AppComponent {
   quickAddTask: string = "";
 
   toDoList: ToDo[] = [
-    {task: "Fold clothes", completed: false, duration: 2},
-    {task: "Meal prep", completed: true, duration: 2},
-    {task: "Eat meal prep", completed: false, duration: 2},
-    {task: "Bang head on wall", completed: true, duration: 2},
-    {task: "Get a job", completed: false, duration: 2},
+    {task: "Fold clothes FALSE", completed: false, duration: 2},
+    {task: "Meal prep TRUE", completed: true, duration: 2},
+    {task: "Eat meal prep FALSE", completed: false, duration: 2},
+    {task: "Bang head on wall TRUE", completed: true, duration: 2},
+    {task: "Get a job FALSE", completed: false, duration: 2},
   ]
-
+  // addTask ():void{
+  //   this.formToDo.
+  // }
 
 
   // YOU WERE TRYING TO BE FANCY BUT YOU ARE A FOOL
